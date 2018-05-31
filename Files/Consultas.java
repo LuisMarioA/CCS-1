c4a71e92fab2e0e2c932596626ace73a6f561dc1
LuisMarioA <LuisMarioA@users.noreply.github.com>
2018-05-31 16:22:51 -0500

    Consultas inicio sesión perfil

--- Files/Consultas.java
+++ Files/Consultas.java
@@ -34,7 +34,7 @@ public class Consultas extends Conexion{
         PreparedStatement pst=null;
         ResultSet rs=null;
         try {
-            String consulta="select *  from empleado where correo=?";
+            String consulta="select * from empleado where correo=?";
             pst = getConnection().prepareStatement(consulta);
             pst.setString(1, usuario);
             rs = pst.executeQuery();
@@ -45,11 +45,45 @@ public class Consultas extends Conexion{
                 String apellido2=rs.getString("apellido_m");
                 String correo=rs.getString("correo");
                 int area=rs.getInt("id_area");
+                int rool=rs.getInt("id_rool");
                 String nombreArea="";
                 String perfil="";
                 if(area==1){
                     nombreArea="Análisis";
-                    perfil="Analista";
+                    if(rool==1)
+                        perfil="Analista";
+                    else
+                        perfil="Jefe Analista";
+                }else if(area==2){
+                    nombreArea="Desarrollo";
+                    if(rool==7)
+                        perfil="Jefe de Desarrollo";
+                    else
+                        perfil="Desarrollador";
+                }else if(area==3){
+                    nombreArea="Arquitectura";
+                    if(rool==8)
+                        perfil="Jefe Aarquitecto";
+                    else
+                        perfil="Arquitecto";
+                }else if(area==4){
+                    nombreArea="Bases de datos";
+                    if(rool==9)
+                        perfil="Jefe Bases de Datos";
+                    else
+                        perfil="Soporte-Base";
+                }else if(area==5){
+                    nombreArea="Pruebas - Calidad";
+                    if(rool==10)
+                        perfil="Jefe Pruebas - Calidad";
+                    else
+                        perfil="Pruebas-Calidad";
+                }else if(area==6){
+                    nombreArea="Master";
+                    if(rool==11)
+                        perfil="Lider de Proyecto";
+                    else
+                        perfil="Esto no pasa";
                 }
                 String resultado= id + "," + nombre + "," + apellido + "," +apellido2+ "," +correo+ "," +nombreArea+","+perfil;
                 return resultado;
@@ -68,9 +102,38 @@ public class Consultas extends Conexion{
         return null;
     }
     
+    public String obtenerJefe (String usuario){
+        PreparedStatement pst=null;
+        ResultSet rs=null;
+        try {
+            String consulta="select id_rool from empleado where correo=?";
+            pst = getConnection().prepareStatement(consulta);
+            pst.setString(1, usuario);
+            rs = pst.executeQuery();
+            if(rs.absolute(1)){
+                int id=rs.getInt("id_rool");
+                if(id>5)
+                    return "Jefe";
+                else
+                    return "Empleado";  
+            }
+        } catch (Exception e) {
+            System.out.println("Error:" +e);
+        }finally{
+            try {
+                if(getConnection()!= null) getConnection().close();
+                if(pst != null) pst.close();
+                if(rs !=null) rs.close();
+            } catch (Exception e) {
+                System.out.println("Error:" +e);
+            }
+        }
+        return null;
+    }
+    
     public static void main(String[] args) {
-        Consultas co= new Consultas();
-        System.out.println(co.infoPerfil("correo@gmail.com"));
+        Consultas con= new Consultas();
+        System.out.println(con.obtenerJefe("fer@gmail.com"));
         
     }
 }
